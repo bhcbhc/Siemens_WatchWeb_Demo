9c6cc78a56627822728a0fa7d85f8f20ed6ef999
bhcbhc <995265694@qq.com>
2017-05-26 09:05:49 +0800

    final_Demo

--- app.js
+++ app.js
@@ -6,112 +6,155 @@ require.config({
         //utils
         "initWeb": './src/js/utils/widgetUtil',
         "getTree": './src/js/utils/treeUtil',
-        'mapBase': './src/js/map/mapBase',
-        "requestAsync": './src/js/utils/requestAsync'
+        "requestAsync": './src/js/utils/requestAsync',
+        'timer': './src/js/utils/localTimer',
+        'init': './src/js/core/init',
+        'mapBase': './src/js/map/mapBase'
     }
 
 });
-require(["initWeb", "mapBase", "requestAsync", "getTree"], function (initWeb, mapBase, requestAsync, getTree) {
+require(["init", "requestAsync", 'initWeb'], function (init, requestAsync, initWeb) {
     "use strict";
 
-    //初始化左侧菜单栏
-    $('#main-menu').kendoPanelBar({});
-    var menu = $('#main-menu').data('kendoPanelBar');
-    $.getJSON('src/js/menu.json').then(function (d) {
-        var data = [];
-        for (var key in d) {
-            data.push(d[key]);
-        }
-        var tree = getTree.getTree(data, 0);
-        console.log(tree);
-        menu.setOptions({
-            dataSource: tree
-        });
-    });
 
-
-    //初始化地图
-    var basicConf = {
-        view: 'map-container',
-        //116.4615, 39.97817
-        point: [39.97817, 116.4615],
-        zoom: 19,
-        mark: [
-            {
-                url: "src/images/siemens/intersections.png",
-                point: [39.97817, 116.4615],
-                id: "machine1"
-            },
-            {
-                url: "src/images/siemens/intersections.png",
-                point: [39.97909, 116.46120],
-                id: "machine2"
-            }
-        ]
-    };
-    initWeb.initInterface("menu");
-    var map = mapBase.initMap(basicConf);
+    var map = init.init();
 
     //经纬度，图片路径
-    var icon = mapBase.addIcon([39.97805, 116.46181], "src/images/siemens/intersections.png");
-    map.addLayer(icon);
-
+    /*    var icon = mapBase.addIcon([39.97805, 116.46181], "src/images/siemens/intersections.png");
+     map.addLayer(icon);*/
 
 
     //test mark
-    var pos = ol.proj.fromLonLat([116.46625, 39.98247]);
-    var marker = new ol.Overlay({
-        position: pos,
-        positioning: 'center-center',
-        element: document.getElementById('marker'),
-        stopEvent: false
-    });
-    map.addOverlay(marker);
+    /*    var pos = ol.proj.fromLonLat([116.46625, 39.98247]);
+     var marker = new ol.Overlay({
+     position: pos,
+     positioning: 'center-center',
+     element: document.getElementById('marker'),
+     stopEvent: false
+     });
+     map.addOverlay(marker);*/
 
     //BMap
 
+    //初始化信息板
+    //initWeb.initDashBoard(document.getElementById('leftToolBar'));
+
+    //demo信息板
+    var timer1 = setInterval(function () {
+        requestAsync.post({
+            url: 'http://192.168.0.4:8080/WatchWeb_NEW/Main',
+            //url: './monitor.json',
+            data: {"id": 1}
+        }).then(function (data) {
+            if(!data){
+                return
+            }
+            var template = kendo.template($('#db_model').html()),
+                result = template(formatData(data));
+            $('#monitor1').find('table').remove();
+            $('#monitor1').append(result);
+        })
+    }, 250);
+
+    var timer2 = setInterval(function () {
+        requestAsync.post({
+            url: 'http://192.168.0.4:8080/WatchWeb_NEW/Main',
+            //url: './monitor.json',
+            data: {"id": 2}
+        }).then(function (data) {
+            if(!data){
+                return
+            }
+            var template = kendo.template($('#db_model').html()),
+                result = template(formatData(data));
+            $('#monitor2').find('table').remove();
+            $('#monitor2').append(result);
+        })
+    }, 250);
+
+
+    function formatData(data) {
+        var formatedData = {};
+        for (var item in data) {
+            formatedData[item] = data[item];
+        }
+        switch (data.mode) {
+            case 2:
+                formatedData.mode = "本地";
+                break;
+            case 3:
+                formatedData.mode = "手动";
+                break;
+            case 4:
+                formatedData.mode = "中心控制";
+        }
+        if (data.online) {
+            formatedData.online = "在线";
+        } else {
+            formatedData.online = "离线";
+        }
+        return formatedData;
+    }
 
     //test popup
     var popup = new ol.Overlay({
-        element: document.getElementById('popup')
+        element: document.getElementById('popup'),
+        positioning: 'bottom-center',
+        offset: [0, -50]
     });
     map.addOverlay(popup);
 
+
     map.on('click', function (evt) {
-        var element = popup.getElement();
-        var coordinate = evt.coordinate;
-        var hdms = ol.coordinate.toStringHDMS(ol.proj.transform(
-            coordinate, 'EPSG:3857', 'EPSG:4326'));
-
-        $(element).popover('destroy');
-        popup.setPosition(coordinate);
-        // the keys are quoted to prevent renaming in ADVANCED mode.
-
-        $('#popup').attr('title', '地理位置:\n' + hdms);
-
-        requestAsync.post({
-            url: "http://192.168.0.4:8080/WatchWeb_NEW/Main",
-            data: {"id": 1}
-        }).then(function (data) {
-            console.info("输出结果%o", data);
-            //var value = "id:" + data.id + "mode" + data.mode + "time:" + data.time;
-            $(element).popover({
-                'placement': 'bootom',
-                'animation': false,
-                'html': true,
-                'content': '<p>设备数据测试：</p>' +
-                '<table class="table table-bordered">' +
-                '<tbody>' +
-                '<tr><td>ID</td><td>' + data.id + '</td><td>Mode</td><td>' + data.mode + '</td></tr>' +
-                '<tr><td>Online</td><td>' + data.online + '</td><td>Plan</td><td>' + data.plan + '</td></tr>' +
-                '<tr><td>Stage</td><td>' + data.mode + '</td><td>State</td><td>' + data.state + '</td></tr>' +
-                '</tbody>' +
-                '</table>' +
-                '<span>最后更新时间：' + data.time + '</span>'
-            });
-            $(element).popover('show');
+        var feature = map.forEachFeatureAtPixel(evt.pixel, function (feature) {
+            return feature;
         });
+        var element = popup.getElement();
+        if (feature) {
+            var id = parseInt(feature.get('id')),
+                name = feature.get('name'),
+                coordinates = feature.getGeometry().getCoordinates();
+            console.log(id);
+            console.log(name);
+            popup.setPosition(coordinates);
+            requestAsync.post({
+                url: "http://192.168.0.4:8080/WatchWeb_NEW/Main",
+                // url: './monitor.json',
+                data: {"id": id}
+            }).then(function (data) {
+                //$(element).popover('destroy');
+                console.log(element);
+                console.info("输出结果%o", data);
+                $(element).empty();
+                $(element).popover({
+                    'placement': 'right',
+                    'animation': true,
+                    'html': true,
+                    'content': '<table class="table table-bordered">' +
+                    '<tbody>' +
+                    '<tr><td>ID</td><td>' + data.id + '</td><td>模式</td><td>' + data.mode + '</td></tr>' +
+                    '<tr><td>在线</td><td>' + data.online + '</td><td>方案</td><td>' + data.plan + '</td></tr>' +
+                    '<tr><td>阶段</td><td>' + data.mode + '</td><td>状态</td><td>' + data.state + '</td></tr>' +
+                    '<tr><td>时间</td><td colspan="3">' + data.time + '</td></tr>' +
+                    '</tbody>' +
+                    '</table>'
+                });
+                $(element).popover('show');
+            });
+        }
+        else {
+            $(element).popover('destroy');
+        }
     });
 
-
+    // change mouse cursor when over marker
+    map.on('pointermove', function (e) {
+        if (e.dragging) {
+            $(element).popover('destroy');
+            return;
+        }
+        // var pixel = map.getEventPixel(e.originalEvent);
+        // var hit = map.hasFeatureAtPixel(pixel);
+        // map.getTarget().style.cursor = hit ? 'pointer' : '';
+    });
 });
