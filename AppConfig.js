9b030bb49ec23d56cd277c67ab7ec9533b8ac0a7
bhcbhc <995265694@qq.com>
2017-07-05 09:40:46 +0800

    configuration file

--- AppConfig.js
+++ AppConfig.js
@@ -11,6 +11,12 @@
         ServiceRoute: "/WatchWeb_NEW/Main",
         ////////////////////////////
 
+        ///////////////////////////
+        ////BaiDuDataService///////
+        ///////////////////////////
+        //    http://192.168.0.180:8080/WatchWeb_NEW/Main
+        BDServerAddress:"./src/allDataTest/routeOrigion2.json",
+
         /////////////////////////////
         ///Map Basic Configuration///
         /////////////////////////////
@@ -21,33 +27,43 @@
                     collapsible: false
                 })
             }),
-            logo: false, //{src:'../test.png',href:'http://www.baidu.com'}
+            logo: false, //{src:'../test.png',     href:'http://www.baidu.com'}
             layers: [
                 new ol.layer.Tile({
                     source: new ol.source.XYZ({
-                        url: 'http://{a-c}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+                        url: './src/tiles/Wangjing area/{z}/{x}/{y}.png'
                     })
                 })
+/*                new ol.layer.Tile({
+                    source: new ol.source.TileDebug({
+                        projection: 'EPSG:3857',
+                        tileGrid: new ol.source.OSM().getTileGrid()
+                    })
+                }) */
             ],
             view: {
-                //投影设置 4326等同于WGS8坐标系，3857等同于900913墨卡托投影，openlayer web默认为墨卡托
-              //  projection: 'EPSG:3857',
-                center:[39.98362,116.46628],//地图中心
-                extent: [116.44, 39.97, 116.5, 40.01],//限制地图中心范围
+                //投影设置 4326等同于WGS84坐标系，3857等同于900913墨卡托投影，openlayer web默认为墨卡托
+              //  projection: 'EPSG:4326',
+                center:[39.98254,116.46640],//地图中心
+                //  extent: [39.97,116.44,40.01,116.5],//限制地图中心范围
                 zoom: 19,//地图默认zoom等级
                 minZoom: 13,
-                maxZoom: 17
+                maxZoom: 19
             },
             marks: [
                 {
                     url: "src/images/siemens/intersections.png",
-                    point: [39.98269, 116.46636],
-                    id: "1"
+                    point: [39.98256,116.46641],
+                    //相对于点的偏移，默认[0.5,0.5]
+                    //position:[0.5,0.5],
+                    //缩小等级
+                    scale:18,
+                    id: 1
                 },
                 {
                     url: "src/images/siemens/intersections.png",
-                    point: [39.98495, 116.46287],
-                    id: "2"
+                    point: [39.98485,116.46286],
+                    id: 2
                 }
             ]
         }
