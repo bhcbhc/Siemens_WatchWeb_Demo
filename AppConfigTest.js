b637dda78c7e52887ee955b63adf6a384504fd7e
bhcbhc <995265694@qq.com>
2017-07-25 14:26:05 +0800

    interface  TEST

---
+++ AppConfigTest.js
@@ -0,0 +1,68 @@
+/**
+ * Created by Ninghai on 2017/7/25.
+ */
+
++function () {
+    "use strict";
+    window.AppConfig = {
+        ////////////////////////////
+        //////数据接口地址配置 /////
+        ////////////////////////////
+        serverAddress: "file:///E:/aic-tech/Project/Siemens/Siemens_WatchWeb_Demo/src/allDataTest",
+
+        //百度点坐标数据接口地址
+        BDPointsAddress: "/linkPointsData.json",
+
+        //信号机数据接口地址
+        monitorAddress: "/monitor.json",
+
+        //获取所有路口id和坐标
+        allStreamAddress: "/allRoutes.json",
+
+        //link-message接口地址
+        linkMessageAddress: "/chartData.json",
+
+        /////////////////////////////
+        ///openlayers地图基本配置///
+        /////////////////////////////
+        mapConfig: {
+            target: 'map-container',
+            controls: ol.control.defaults({
+                attributionOptions: /** @type {olx.control.AttributionOptions} */ ({
+                    collapsible: false
+                })
+            }),
+            logo: false,
+            layers: [
+                new ol.layer.Tile({
+                    source: new ol.source.XYZ({
+                        url: './src/tiles/Wangjing area/{z}/{x}/{y}.png'
+                    })
+                })
+                /*                new ol.layer.Tile({
+                 source: new ol.source.TileDebug({
+                 projection: 'EPSG:3857',
+                 tileGrid: new ol.source.OSM().getTileGrid()
+                 })
+                 }) */
+            ],
+            view: {
+                //投影设置 4326等同于WGS84坐标系，3857等同于900913墨卡托投影，openlayer web默认为墨卡托
+                //  projection: 'EPSG:4326',
+                center: [39.98254, 116.46640],//地图中心
+                //  extent: [39.97,116.44,40.01,116.5],//限制地图中心范围
+                zoom: 19,//地图默认zoom等级
+                minZoom: 13,
+                maxZoom: 19
+            },
+            monitorParam: {
+                url: "src/images/siemens/intersections.png",
+                //point: [39.98256, 116.46641],  //坐标
+                //position:[0.5,0.5],  //相对于点的偏移，默认[0.5,0.5]
+                //id: 1,       //id标识
+                //缩小等级
+                scale: 18
+            }
+        }
+    }
+}();
